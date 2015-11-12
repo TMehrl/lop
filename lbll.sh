6f713f0f2f812c816e295bf7e1b1bf26e41efb72
Timon <t.mehrling@gmail.com>
2015-11-12 10:32:10 +0000

    Some corrections.

--- lbll.sh
+++ lbll.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 basename=lop
 echo "============================================ pdflatex $basename.tex ============================================" && \
-/usr//local/texlive/2012/bin/x86_64-darwin/pdflatex "$basename.tex" && \
+pdflatex "$basename.tex" && \
 echo "============================================ bibtex ${basename}1.aux ============================================" && \
 bibtex "${basename}1.aux" && \
 echo "============================================ bibtex ${basename}2.aux ============================================" && \
@@ -9,4 +9,4 @@ bibtex "${basename}2.aux" && \
 echo "============================================ bibtex ${basename}3.aux ============================================" && \
 bibtex "${basename}3.aux" && \
 echo "============================================ pdflatex $basename.tex ============================================" && \
-/usr//local/texlive/2012/bin/x86_64-darwin/pdflatex "$basename.tex" 
+pdflatex "$basename.tex" 
