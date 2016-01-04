05afd9ba409937e543a5632407c5a53284ec5f35
Timon Mehrling <timon.mehrling@desy.de>
2016-01-04 20:02:42 +0100

    Added Proceeding Pathak et al. 2015

--- lbll.sh
+++ lbll.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 basename=lop
 echo "============================================ pdflatex $basename.tex ============================================" && \
-pdflatex "$basename.tex" && \
+/usr/local/texlive/2012/bin/x86_64-darwin/pdflatex "$basename.tex" && \
 echo "============================================ bibtex ${basename}1.aux ============================================" && \
-bibtex "${basename}1.aux" && \
+/usr/local/texlive/2012/bin/x86_64-darwin/bibtex "${basename}1.aux" && \
 echo "============================================ bibtex ${basename}2.aux ============================================" && \
-bibtex "${basename}2.aux" && \
+/usr/local/texlive/2012/bin/x86_64-darwin/bibtex "${basename}2.aux" && \
 echo "============================================ bibtex ${basename}3.aux ============================================" && \
-bibtex "${basename}3.aux" && \
+/usr/local/texlive/2012/bin/x86_64-darwin/bibtex "${basename}3.aux" && \
 echo "============================================ pdflatex $basename.tex ============================================" && \
-pdflatex "$basename.tex" 
+/usr/local/texlive/2012/bin/x86_64-darwin/pdflatex "$basename.tex" 
